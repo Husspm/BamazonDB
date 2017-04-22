fad0867d9781b6ed71766c80d1e37cdc9734bbf6
Patrick Hussey <husspm@gmail.com>
2017-04-21 22:38:00 -0400

    inventory.js finished, will modify to become manager version

--- inventory.js
+++ inventory.js
@@ -12,17 +12,10 @@ connection.connect(function(err) {
     if (err) throw err;
     else {
         console.log("Welcome you are connected to the bamazonDB as id + " + connection.threadId);
-        createItem();
+        createProduct();
     }
 });
-//contructor for adding items into the db
-function Product(name, department, price, quantity) {
-    this.product_name = name;
-    this.department_name = department;
-    this.price = price;
-    this.quantity = quantity;
-    return this;
-}
+
 //function to be called after successful connection
 function createProduct() {
     ask.prompt([{
@@ -46,28 +39,58 @@ function createProduct() {
             message: "Enter how many to add"
         }
     ]).then(function(name) {
-        addProduct(name);
+        //addProduct(name);
+        testValues(name);
     });
 }
-//function to actually add info gathered from createProduct function and add it to the db
+//testing the values before attempting to update the database, any info missing will return to the createProduct function
+function testValues(product) {
+    console.log("Testing your values...");
+    //there is probably a smarter way to do this, but this is mostly working so I am gonna run with it
+    var passedTestOne = true;
+    var passedTestTwo = false;
+    for (var key in product) {
+        if (product[key] === "" || product[key] === undefined) {
+            passedTestOne = false;
+            console.log(key + " is a required field");
+        } else {
+            passedTestTwo = true;
+        }
+    }
+    if (passedTestOne && passedTestTwo) {
+        console.log("Tests Passed, content can now be added to the inventory list");
+        addProduct(product);
+    } else {
+        console.log("Some information was missing, please try again");
+        createProduct();
+    }
+}
+//function to add info gathered from createProduct function that passed testValues function and add it to the db
 function addProduct(product) {
-    connection.query("INSERT INTO products SET ?", new Item(product.name, product.department, product.price, product.quantity),
+    connection.query("INSERT INTO products SET ?", new Product(product.name, product.department, product.price, product.quantity),
         function(err, result) {
             if (err) throw err;
-            //if no error occured tell user the item was added
+            //if no error occured tell user the item was added and display inventory
             if (!err)
-                console.log("Your item has been added. Good luck!");
+                console.log("Your item has been added. Thank You!");
             displayUpdates();
         });
+}
+//contructor for adding items into the db
+function Product(name, department, price, quantity) {
+    this.product_name = name;
+    this.department_name = department;
+    this.price = price;
+    this.quantity = quantity;
+    return this; //return to caller 
 }
 //displays the info once itemed successfully added
 function displayUpdates() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         for (var index = 0; index < res.length; index++) {
-            for (var key in res[index]) {
-                console.log(key.toUpperCase() + " " + res[index][key]);
-            } // ends key in res[index] loop
+            //was using a for key in res[index] loop nested in this loop but the print out was too sloppy
+            console.log(res[index].department_name, res[index].product_name, res[index].price, res[index].quantity);
         } // ends var index for loop
     }); // ends mysql call
     createProduct(); //start the whole process over
