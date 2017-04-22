8dc21a057f397002970a1a92f6642a7e7a310d44
Patrick Hussey <husspm@gmail.com>
2017-04-22 01:52:40 -0400

    pushing, but I gotta undo to make that video

--- customer.js
+++ customer.js
@@ -5,7 +5,7 @@ var connection = db.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "qaz123wsx456",
+    password: "",
     database: "BamazonDB"
 });
 connection.connect(function(err) {
